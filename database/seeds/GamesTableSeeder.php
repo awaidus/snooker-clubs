da4702651a6e661b2588116ad2b998493013f1aa
Awaidus <m.awaidus@gmail.com>
2017-03-23 20:51:14 +0500

    Defatul String value changed for database. .bash_prfofile and .bashrc files are added.

--- database/seeds/GamesTableSeeder.php
+++ database/seeds/GamesTableSeeder.php
@@ -8,6 +8,7 @@ class GamesTableSeeder extends Seeder
     {
         $game = new \App\Game([
             'game_table_id' => '1',
+            'manager_id' => 1,
             'game_type_id' => '1',
             'player_id' => '1',
             'completed' => '1',
@@ -19,6 +20,7 @@ class GamesTableSeeder extends Seeder
 
         $game = new \App\Game([
             'game_table_id' => '2',
+            'manager_id' => 1,
             'game_type_id' => '4',
             'player_id' => '3',
             'completed' => '0',
@@ -30,6 +32,7 @@ class GamesTableSeeder extends Seeder
 
         $game = new \App\Game([
             'game_table_id' => '3',
+            'manager_id' => 1,
             'game_type_id' => '3',
             'player_id' => '2',
             'completed' => '1',
@@ -41,6 +44,7 @@ class GamesTableSeeder extends Seeder
 
         $game = new \App\Game([
             'game_table_id' => '4',
+            'manager_id' => 1,
             'game_type_id' => '1',
             'player_id' => '1',
             'completed' => '0',
@@ -52,6 +56,7 @@ class GamesTableSeeder extends Seeder
 
         $game = new \App\Game([
             'game_table_id' => '8',
+            'manager_id' => 1,
             'game_type_id' => '1',
             'player_id' => '2',
             'completed' => '1',
@@ -63,6 +68,7 @@ class GamesTableSeeder extends Seeder
 
         $game = new \App\Game([
             'game_table_id' => '7',
+            'manager_id' => 1,
             'game_type_id' => '2',
             'player_id' => '1',
             'completed' => '1',
@@ -74,6 +80,7 @@ class GamesTableSeeder extends Seeder
 
         $game = new \App\Game([
             'game_table_id' => '6',
+            'manager_id' => 1,
             'game_type_id' => '1',
             'player_id' => '2',
             'completed' => '1',
@@ -85,6 +92,7 @@ class GamesTableSeeder extends Seeder
 
         $game = new \App\Game([
             'game_table_id' => '8',
+            'manager_id' => 1,
             'game_type_id' => '1',
             'player_id' => '3',
             'completed' => '0',
@@ -96,6 +104,7 @@ class GamesTableSeeder extends Seeder
 
         $game = new \App\Game([
             'game_table_id' => '9',
+            'manager_id' => 1,
             'game_type_id' => '1',
             'player_id' => '2',
             'completed' => '1',
@@ -108,6 +117,7 @@ class GamesTableSeeder extends Seeder
 
         $game = new \App\Game([
             'game_table_id' => '12',
+            'manager_id' => 1,
             'game_type_id' => '2',
             'player_id' => '2',
             'completed' => '1',
@@ -119,6 +129,7 @@ class GamesTableSeeder extends Seeder
 
         $game = new \App\Game([
             'game_table_id' => '13',
+            'manager_id' => 1,
             'game_type_id' => '1',
             'player_id' => '3',
             'completed' => '0',
@@ -130,6 +141,7 @@ class GamesTableSeeder extends Seeder
 
         $game = new \App\Game([
             'game_table_id' => '14',
+            'manager_id' => 1,
             'game_type_id' => '3',
             'player_id' => '3',
             'completed' => '1',
